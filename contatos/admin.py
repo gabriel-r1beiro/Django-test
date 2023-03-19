8f81a4518370ee863cd1ad25d70470ec3e086559
Gabriel <gabrielsribeiro9999@gmail.com>
2023-03-19 12:41:14 -0300

    Atualizações 19/03
    Alterei os seguintes diretorios: admin.py, models.py, db.sqlite e os templates

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome', 'telefone', 'categoria','data_criacao')
+    list_display_links = ('nome', 'sobrenome',)
+    list_per_page = 10
+
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+class Categoria(models.Model):
+    nome =  models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nome
+
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=50)
+    sobrenome = models.CharField(max_length=100, blank=True)
+    telefone = models.CharField(max_length=15)
+    email = models.CharField(max_length=75, blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+
+    def __str__(self):
+        return self.nome
+
+
