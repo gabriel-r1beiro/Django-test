6f1d93401c18c9834805c756088b3ee60aa05119
Gabriel <gabrielsribeiro9999@gmail.com>
2023-03-21 20:25:08 -0300

    Erro404

--- contatos/views.py
+++ contatos/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Contato
-
 
 def index(request):
     contatos = Contato.objects.all()
@@ -8,9 +8,8 @@ def index(request):
         'contatos': contatos
     })
 
-
 def ver_contato(request, contato_id):
-    contato = Contato.objects.get(id=contato_id)
+    contato = get_object_or_404(Contato, id=contato_id)
     return render(request, 'contatos/ver_contato.html', {
         'contato': contato
     })
