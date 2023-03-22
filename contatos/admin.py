59a44f1cb10677a8896f84af1664ca6738b2efe5
Gabriel <gabrielsribeiro9999@gmail.com>
2023-03-21 21:35:59 -0300

    atualiza

--- contatos/admin.py
+++ contatos/admin.py
@@ -3,10 +3,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'telefone', 'categoria','data_criacao')
+    list_display = ('nome', 'sobrenome', 'telefone',
+                     'categoria','data_criacao','mostra'
+                    )
     list_display_links = ('nome', 'sobrenome',)
     list_per_page = 10
     search_fields  = ('nome','sobrenome', 'categoria')
-
+    list_editable = ('telefone', 'mostra')
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
